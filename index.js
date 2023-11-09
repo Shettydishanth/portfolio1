c45f07bc9c9dee43197c04ce6e3b3ab139caae0e
dishanth <shettysamarth738@gmail.com>
2023-11-09 12:13:27 +0530

    added scrool reveal

---
+++ index.js
@@ -0,0 +1,7 @@
+ScrollReveal({ 
+    reset: true ,
+    distance:'180px',
+    duration:2000,
+    delay:50                 
+});
+ScrollReveal().reveal('.ai img',{delay:600,origin:'top',interval:200});
