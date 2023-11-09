dcf16121b8021edd83e15f1768e046f88ade6287
dishanth <shettysamarth738@gmail.com>
2023-11-09 12:27:11 +0530

    added scroll reveal to more

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ ScrollReveal({
     delay:50                 
 });
 ScrollReveal().reveal('.ai img',{delay:600,origin:'top',interval:200});
+ScrollReveal().reveal('.image-container',{delay:600,origin:'left',interval:200});
+ScrollReveal().reveal('.abtme',{delay:600,origin:'top',interval:200});
+ScrollReveal().reveal('.side',{delay:600,origin:'right',interval:200});
